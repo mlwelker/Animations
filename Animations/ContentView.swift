f88252fcb6c04abc5ec6963209abe3fa02c78dde
mlwelker <nutria.watts.0g@icloud.com>
2023-08-09 18:49:32 -0500

    circle button animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -2,14 +2,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap me") {
+//            animationAmount += 1
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(.interpolatingSpring(stiffness: 90, damping: 6.5), value: animationAmount)
     }
 }
 
