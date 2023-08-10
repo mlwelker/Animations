59864b2216e9fae1b2bd5ebd67826cd5a74c9dc7
mlwelker <nutria.watts.0g@icloud.com>
2023-08-10 13:06:31 -0500

    drag gesture snaking text

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -2,23 +2,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged{ dragAmount = $0.translation }
-                    .onEnded{ _ in
-                        withAnimation {
-                            dragAmount = .zero
-                        }
-                    }
-            )
-            .animation(.spring(), value: dragAmount)
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) {
+                Text(String(letters[$0]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double($0) / 20),
+                        value: dragAmount
+                    )
+                
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
